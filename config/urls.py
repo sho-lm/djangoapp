818e3ef3a37bf9414fe0c45faf9825893661bd11
sho <s_h2m27@yahoo.co.jp>
2025-04-29 22:18:21 +0900

    djangoが作成したフォルダ構成を変更
    settings.pyなどはconfigフォルダにまとめる
    appはappフォルダにまとめる、staticとtemplateも同様
    フォルダ構成変更に伴ってパスなども変更

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('budgets/', include('app.budgets.urls')), # budgetsのurlを作成したら有効化する
 ]
