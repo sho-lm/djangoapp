086f3d781e9904cc4aea5bbec42ab036e0186663
sho <s_h2m27@yahoo.co.jp>
2025-04-30 08:28:25 +0900

    C:/Program Files/Git/パスのアクセスに関する変更
    画面表示後のurlをそろえるために、/のパスのアクセスはリダイレクトさせるように変更

--- app/budgets/views.py
+++ app/budgets/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
 # Create your views here.
+
+# /パスでアクセスしたとき用のリダイレクト
+def index(request):
+    return redirect('/budgets/')
+
+# 一覧画面
 def top(request):
     return HttpResponse('Hello World')
